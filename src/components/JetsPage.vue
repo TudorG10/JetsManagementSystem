03a2082a1fa357678982e31bdc622ea51ea5246a
TudorG10 <gurau.tudor@yahoo.com>
2019-01-13 18:53:58 -0500

    Colours have appeared, starting to look decent

--- src/components/JetsPage.vue
+++ src/components/JetsPage.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="JetsPage">
-
-      <table>
-          <caption>Attackers</caption>
-          <tr>
-              <td>
-                  <img :src="tData.data_set.team.images[5]._value">
-              </td>
-          </tr>
-      </table>
+      <div style="background-color: lightgrey; padding: 25px; border-bottom: 10px solid blue; width: 100%;">
+        <table style="border-collapse: separate; border-spacing: 15px 0px;">
+            <tr>
+                <td style="min-width: 200px; min-height: 200;" rowspan="2">
+                    <img :src="tData.data_set.team.images[3]._value">
+                </td>
+                <td style="font-size: 40px; min-width: 500px; text-align: left; background-color: #e6e6e6; border: 2px solid white; box-shadow: 10px 10px #888888; padding-left: 20px" rowspan="2"><span style="font-weight: bold;">Jets</span> Winnipeg</td>
+                <td class="result-label">Wins</td>
+                <td class="result-label">Losses</td>
+                <td class="result-label">Rank</td>
+                <td class="result-label">Points</td>
+            </tr>
+            <tr>
+                <td class="result-value">{{tData.data_set.team.standing.wins}}</td>
+                <td class="result-value">{{tData.data_set.team.standing.losses}}</td>
+                <td class="result-value">{{tData.data_set.team.standing.division_rank}}</td>
+                <td class="result-value">{{tData.data_set.team.standing.points}}</td>
+            </tr>
+        </table>
+      </div>
       <table id="attackerPlayerList">
+           <caption>Attackers</caption>         
           <tr>
             <th></th>
             <th>No</th>
@@ -92,5 +104,21 @@ export default {
       font-family: Arial, Helvetica, sans-serif;
       color: #000000;
       background-color: #FFFFFF;
+  }
+  .JetsPage{
+      background-color: white;
+  }
+  .result-value{
+      font-size: 24px;
+      font-weight: bold;
+  }
+  .result-label{
+      min-width: 100px; 
+      font-size: 24px;
+      vertical-align: bottom;
+      background-color: blue;
+      color: #FFFFFF;
+      font-weight: bold;
+      border-spacing: 50px;
   }
 </style>
